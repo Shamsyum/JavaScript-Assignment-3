897105141d94f556e2c9677936363ac6fb9fff84
Shams Ur Rahman <shams.programmer@gmail.com>
2019-10-10 11:23:31 +0500

    Solved Question no 2 of the assignment in index.js file

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
+
+/*
+
 // Question: 1
 var eduQualifications = ["SSC", "HSC", "BSC", "BS", "BCOM", "MS", "M.Phil", "PhD"];
 for(i=0; i<eduQualifications.length; i++){
     document.write(eduQualifications[i] + "<br>");
 }
+
+*/
+
+//Question: 2
+var students = ["Michail", "John", "Tony"];
+var obtainedMarks = [320, 230, 480];
+var totalMarks = 500;
+var michailPercentage = ( obtainedMarks[0] * 100 ) / totalMarks;
+var johnPercentage = ( obtainedMarks[1] * 100 ) / totalMarks;
+var tonyPercentage = ( obtainedMarks[2] * 100 ) / totalMarks;
+
+document.write("Score of " + students[0] + " is " + obtainedMarks[0] + " Percentage " + michailPercentage + "%" + "<br>");
+document.write("Score of " + students[1] + " is " + obtainedMarks[0] + " Percentage " + johnPercentage + "%" + "<br>");
+document.write("Score of " + students[2] + " is " + obtainedMarks[0] + " Percentage " + tonyPercentage + "%");
