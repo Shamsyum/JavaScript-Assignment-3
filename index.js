f655f613dc8bc59cf3aeff3e09ee041e820e5778
Shams Ur Rahman <shams.programmer@gmail.com>
2019-10-10 10:52:34 +0500

    Added two files index.html and index.js and solved the first question of the assignment

---
+++ index.js
@@ -0,0 +1,5 @@
+// Question: 1
+var eduQualifications = ["SSC", "HSC", "BSC", "BS", "BCOM", "MS", "M.Phil", "PhD"];
+for(i=0; i<eduQualifications.length; i++){
+    document.write(eduQualifications[i] + "<br>");
+}
