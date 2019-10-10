aa4cc4c63bc84872ef351a2d4fc9e63e4c937850
Shams Ur Rahman <shams.programmer@gmail.com>
2019-10-10 12:02:31 +0500

    Solved/Coded the question no 3 of the assignment in index.js file

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ for(i=0; i<eduQualifications.length; i++){
     document.write(eduQualifications[i] + "<br>");
 }
 
-*/
 
 //Question: 2
 var students = ["Michail", "John", "Tony"];
@@ -20,3 +19,31 @@ var tonyPercentage = ( obtainedMarks[2] * 100 ) / totalMarks;
 document.write("Score of " + students[0] + " is " + obtainedMarks[0] + " Percentage " + michailPercentage + "%" + "<br>");
 document.write("Score of " + students[1] + " is " + obtainedMarks[0] + " Percentage " + johnPercentage + "%" + "<br>");
 document.write("Score of " + students[2] + " is " + obtainedMarks[0] + " Percentage " + tonyPercentage + "%");
+
+*/
+//Question: 3
+var colors = ["Green", "yellow", "red"];
+alert(colors);
+
+var colorAtStart = prompt("Which color you want to add to the list at begining");
+colors.unshift(colorAtStart);
+alert(colors);
+
+colors.unshift("White", "Black");
+alert(colors);
+
+colors.shift();
+alert(colors);
+
+colors.pop();
+alert(colors);
+
+var indexPositionToAddNewElement = prompt("At what position you want to add new color?");
+var newElement = prompt("Which color you want to add?");
+colors.splice(indexPositionToAddNewElement, 0, newElement);
+alert(colors);
+
+var indexPositionToDeleteElements = prompt("At what position you want to delete the elements");
+var numbrOfElementsToDelete = prompt("How many elements to delete");
+colors.splice(indexPositionToDeleteElements,numbrOfElementsToDelete);
+alert(colors);
